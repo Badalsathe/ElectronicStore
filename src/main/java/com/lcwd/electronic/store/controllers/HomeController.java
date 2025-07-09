92877d497bb1607a48ca9b4deb0a945f6303d580
Badalpatil <sathebadal8@gmail.com>
2025-07-09 12:17:43 +0530

    Initial commit

---
+++ src/main/java/com/lcwd/electronic/store/controllers/HomeController.java
@@ -0,0 +1,19 @@
+package com.lcwd.electronic.store.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping ("/test")
+public class HomeController
+{
+
+    @GetMapping("/msg")
+    public String getMsg()
+    {
+
+        return "This Is Electronic Store";
+    }
+
+}
